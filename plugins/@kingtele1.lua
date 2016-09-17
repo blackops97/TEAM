2f5605c57d8294687c8098492f1093940f7af511
blackops97 <blackops97@users.noreply.github.com>
2016-09-17 18:01:20 +0000

    Update and rename @TH3_Evil.lua to @kingtele1.lua

---
+++ plugins/@kingtele1.lua
@@ -0,0 +1,26 @@
+do 
+
+local function sadik(msg, matches) 
+
+if ( msg.text ) then
+
+  if ( msg.to.type == "user" ) then
+
+return "اذا كنت تريد التحدث مع المطورين اضعط ع احد المعرفات التالي \n ❇ @sadikal_knani10 - @illOlli \n او اذا محظور اضغط هنا \n 💢❇  @twoaselbot - @twsl_BABOT \n  قناة الـسـورس \n   @KINGTELE1\n "
+  end
+   
+end 
+
+-- @kingtele1
+
+end 
+
+return { 
+  patterns = { 
+       "(.*)$"
+  }, 
+  run = sadik, 
+} 
+
+end 
+-- By @KINGTELE1
