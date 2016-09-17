66ded75287a693e894e7ba1c47b0b5a43deecb1a
blackops97 <blackops97@users.noreply.github.com>
2016-09-17 17:43:58 +0000

    Update ar-lock-fwd.lua

--- plugins/ar-lock-fwd.lua
+++ plugins/ar-lock-fwd.lua
@@ -1,52 +1,51 @@
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
-▀▄ ▄▀   BY SAJJADHUSSIEN   (@TH3_Evil)     ▀▄ ▄▀    
-▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
-▀▄ ▄▀   ANTI FWD  :  منع اعاده توجيه      ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
---]]
-do
 
+do
 local function pre_process(msg)
-    
-    --Checking mute
-    local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg)  then
+
+    local fwd = 'mate:'..msg.to.id
+    if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
             delete_msg(msg.id, ok_cb, true)
-            return "done"
-        end
-    
+             send_large_msg(get_receiver(msg), 'اخي اختي/🌝 يمنع عمل اعاده توجيه هنا التزم بالقوانين لتجنب الطرد 🔕\n#User @'..msg.from.username)
+            return "ok"
+end
         return msg
     end
-
-  
-
-
-local function run(msg, matches)
-    chat_id =  msg.to.id
     
-    if is_momod(msg) and matches[1] == 'قفل اعاده توجيه' then
-      
-            
-                    local hash = 'mate:'..msg.to.id
-                    redis:set(hash, true)
-                    return "تم ☑️ قفل 🔒 اعاده توجيه ✋😽"
-  elseif is_momod(msg) and matches[1] == 'فتح اعاده توجيه' then
-      local hash = 'mate:'..msg.to.id
-      redis:del(hash)
-      return "تم ☑️ فتح 🔓 اعاده توجيه ✋😽"
+ local function h_k_a(msg, matches)
+     chat_id = msg.to.id
+local reply_id = msg['id']
+     if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
+         local fwd = 'mate:'..msg.to.id
+         redis:set(fwd, true)
+         local text = 'تم قفل اعاده توجيه \n👮تــم الامـر بواسطه '..msg.from.print_name..'\n🔻Order By @'.. msg.from.username..'\n'
+         return reply_msg(reply_id, text, ok_cb, false)
+         end
+local reply_id = msg['id']
+    if not is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
+    local text= '❌ للمشرفين فقط 😻'
+ return reply_msg(reply_id, text, ok_cb, false)
+end
+local reply_id = msg['id']
+if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
+    local fwd = 'mate:'..msg.to.id
+    redis:del(fwd)
+    local text = 'تم️ فتح اعاده توجيه \n👮تــم الامـر بواسطه : '..msg.from.print_name..'\n🔻Order By @'.. msg.from.username..'\n'
+    return reply_msg(reply_id, text, ok_cb, false)
 end
 
-end
+local reply_id = msg['id']
+if not is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
+local text = '❌ للمشرفين فقط 😻'
+ return reply_msg(reply_id, text, ok_cb, false)
+ end
 
+end
 return {
-    patterns = {
-        '^(قفل اعاده توجيه)$',
-        '^(فتح اعاده توجيه)$'
+    patterns ={
+        '^(قفل) (اعاده توجيه)$',
+        '^(فتح) (اعاده توجيه)$'
     },
-    run = run,
-    pre_process = pre_process
+run = h_k_a,
+pre_process = pre_process 
 }
 end
