090bb69897f18696c2a280d2024d485e9eedf17a
blackops97 <blackops97@users.noreply.github.com>
2016-09-17 17:51:50 +0000

    Update ar-me.lua

--- plugins/ar-me.lua
+++ plugins/ar-me.lua
@@ -1,40 +1,49 @@
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
-▀▄ ▄▀   BY SAJJADHUSSIEN   (@TH3_Evil)     ▀▄ ▄▀    
-▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
-▀▄ ▄▀         ME   :  موقعي               ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
---]]
-do
+do 
+local function jacky(msg, matches) 
+if is_sudo(msg) then 
+return '❇ الايدي : '.. msg.from.id..'\n' 
+..'❇ المعرف : @'..msg.from.username..'\n' 
+..'❇الاســم : '..msg.from.print_name..'\n' 
+..'❇ ايدي الكروب : '..msg.to.id..'\n' 
+..'❇ اسم الكروب : '..msg.to.title..'\n' 
+..'❇ موقعك : انــت المــطور مالتــي🙈♥' 
+end 
+if is_owner(msg) then 
+  return '❇ الايدي : '.. msg.from.id..'\n' 
+..'❇ المعرف : @'..msg.from.username..'\n' 
+..'❇ الاسم : '..msg.from.print_name..'\n' 
+..'❇ ايدي الكروب : '..msg.to.id..'\n' 
+..'❇ اسم الكروب : '..msg.to.title..'\n' 
+..'❇ رتبتك : انــت الــمدير مال الــكــروب🙊❤️' 
+end 
+if is_momod(msg) then 
+return '❇ الايدي : '.. msg.from.id..'\n' 
+..'❇ المعرف : @'..msg.from.username..'\n' 
+..'❇ الاسم : '..msg.from.print_name..'\n' 
+..'❇ ايدي الكروب : '..msg.to.id..'\n' 
+..'❇ اسم الكروب : '..msg.to.title..'\n' 
+..'❇ مــوقعك : انــت ادمــن😍🙊' 
+end 
+if not is_momod(msg) then 
+return '❇ الايدي : '.. msg.from.id..'\n' 
+..'❇ المعرف : @'..msg.from.username..'\n' 
+..'❇ الاسم : '..msg.from.print_name..'\n' 
+..'❇ ايدي الكروب : '..msg.to.id..'\n' 
+..'❇اســم الكروب : '..msg.to.title..'\n' 
+..'❇ مــوقعك : انــت دايــح قــصدي عــضو😹😜' 
+end  
 
-local function run(msg, matches)
-  if matches[1] == 'موقعي' then
-    if is_sudo(msg) then
-    send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return "انت المطور مالتي 😻🙊"
-    elseif is_admin1(msg) then
-    send_document(get_receiver(msg), "./files/me/support.webp", ok_cb, false)
-      return "انت اداري  🌚💭"
-    elseif is_owner(msg) then
-    send_document(get_receiver(msg), "./files/me/owner.webp", ok_cb, false)
-      return "انت مدير المجموعه 🌺😍"
-    elseif is_momod(msg) then
-    send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return "انت ادمن ☺️"
-    else
-    send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return "انت مجرد عضو 😒💔"
-    end
-  end
-end
-
-return {
-  patterns = {
-    "^(موقعي)$",
-    "^(موقعي)$"
-    },
-  run = run
-}
-end
+if ( msg.text == "about" ) then 
+if ( msg.from.username == "#DEV: @illOlli" ) then 
+R = get_receiver(msg) 
+send_large_msg ( R , "Made by @illOlli" ); 
+end 
+end 
+end 
+return { 
+  patterns = { 
+       "^(موقعي)$", 
+  }, 
+  run =jacky,
+} 
+end 
