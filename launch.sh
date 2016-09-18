c318d4361b1d649ef2f6c5bb8bf890ed8baf723b
blackops97 <blackops97@users.noreply.github.com>
2016-09-18 05:55:31 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -126,12 +126,12 @@ else
   
   #Adding some color. By @MehdiHS
    echo -e "\033[38;5;208m"
-   echo -e "     > DEV : SAJJAD HUSSIEN                                          "
-   echo -e "     > DEV : @TH3_Evil                                           "
-   echo -e "     > Bot ID : DEV : @sajjad_aliraqi_bot                       "
-   echo -e "     > Github : https://github.com/blackops97/TH3_Evil    "
-   echo -e "          BY sajjad hussien                                   \033[0;00m"
+   echo -e "     > DEV : BAKEY-SADIK                                          "
+   echo -e "     > DEV : @illOlli                                           "
+   echo -e "     > Bot ID : DEV : @twsl_babot                       "
+   echo -e "     > Github : https://github.com/blackops97/KING_TELE    "
+   echo -e "          BY BAKER-SADIK                                   \033[0;00m"
    echo -e "\e[36m"
   rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TH3_Evil.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/KING_TELE.lua -l 1 -E $@
 fi
