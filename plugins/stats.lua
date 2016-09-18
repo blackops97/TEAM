cd9a3ba36c30e745d431e994767d422c31587d54
blackops97 <blackops97@users.noreply.github.com>
2016-09-18 05:52:00 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (th3evil)",
-	"^[#!/]([Tt]h3evil)"
+    "^[#!/]([Ss]tats) (KINGTELE)",
+	"^[#!/]([Kk]nigtele)"
     }, 
   run = run
 }
