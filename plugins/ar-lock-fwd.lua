513b0c67aaf0afd2cd3afc6e9b64118c4e5f5793
blackops97 <blackops97@users.noreply.github.com>
2016-09-23 13:55:17 +0000

    Update ar-lock-fwd.lua

--- plugins/ar-lock-fwd.lua
+++ plugins/ar-lock-fwd.lua
@@ -5,7 +5,7 @@ local function pre_process(msg)
     local fwd = 'mate:'..msg.to.id
     if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
             delete_msg(msg.id, ok_cb, true)
-             send_large_msg(get_receiver(msg), 'اخي اختي/🌝 يمنع عمل اعاده توجيه هنا التزم بالقوانين لتجنب الطرد 🔕\n#User @'..msg.from.username)
+             send_large_msg(get_receiver(msg), '⚠️ #تنبيه_ممنوع_عمل_اعاده___توجيه_داخل_هذه_المجموعه 🔕\n#User @'..msg.from.username)
             return "ok"
 end
         return msg
@@ -17,7 +17,7 @@ local reply_id = msg['id']
      if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
          local fwd = 'mate:'..msg.to.id
          redis:set(fwd, true)
-         local text = 'تم قفل اعاده توجيه \n👮تــم الامـر بواسطه '..msg.from.print_name..'\n🔻Order By @'.. msg.from.username..'\n'
+         local text = 'تم ☑️ قفل 🔒 اعاده توجيه '..'\n\n'..' 👮 MSG BY : @'..msg.from.username..'\n➖➖➖➖➖➖➖➖➖🙄ID MSG BY : '..msg.from.id..'\n➖➖➖➖➖➖➖➖➖🎩GROUP BY : '..msg.to.title..'\n'
          return reply_msg(reply_id, text, ok_cb, false)
          end
 local reply_id = msg['id']
@@ -29,7 +29,7 @@ local reply_id = msg['id']
 if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
     local fwd = 'mate:'..msg.to.id
     redis:del(fwd)
-    local text = 'تم️ فتح اعاده توجيه \n👮تــم الامـر بواسطه : '..msg.from.print_name..'\n🔻Order By @'.. msg.from.username..'\n'
+    local text = 'تم ☑️ فتح 🔓 اعاده توجيه'..'\n\n'..' 👮 MSG BY : @'..msg.from.username..'\n➖➖➖➖➖➖➖➖➖🙄ID MSG BY : '..msg.from.id..'\n➖➖➖➖➖➖➖➖➖🎩GROUP BY : '..msg.to.title..'\n'
     return reply_msg(reply_id, text, ok_cb, false)
 end
 
