b41838e98b7e2e01523664c4ce0048595df97d2d
blackops97 <blackops97@users.noreply.github.com>
2016-11-24 10:15:10 +0000

    Update ar-lock-fwd.lua

--- plugins/ar-lock-fwd.lua
+++ plugins/ar-lock-fwd.lua
@@ -5,19 +5,19 @@ local function pre_process(msg)
     local fwd = 'mate:'..msg.to.id
     if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
             delete_msg(msg.id, ok_cb, true)
-             send_large_msg(get_receiver(msg), '⚠️ #تنبيه_ممنوع_عمل_اعاده___توجيه_داخل_هذه_المجموعه 🔕\n#User @'..msg.from.username)
+             send_large_msg(get_receiver(msg), 'عزيزي('..msg.from.print_name..')يمنع عمل اعاده توجيه داخل المجموعه التزم بالقوانين لتجنب الطرد معرفك @'..msg.from.username)
             return "ok"
 end
         return msg
     end
     
- local function h_k_a(msg, matches)
+ local function sadikal_knani10(msg, matches)
      chat_id = msg.to.id
 local reply_id = msg['id']
      if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
          local fwd = 'mate:'..msg.to.id
          redis:set(fwd, true)
-         local text = 'تم ☑️ قفل 🔒 اعاده توجيه '..'\n\n'..' 👮 MSG BY : @'..msg.from.username..'\n➖➖➖➖➖➖➖➖➖🙄ID MSG BY : '..msg.from.id..'\n➖➖➖➖➖➖➖➖➖🎩GROUP BY : '..msg.to.title..'\n'
+         local text = 'تم ☑️ قفل 🔒 اعاده توجيه '..'\n\n'..msg.to.title..'\n✈️MSG BY :-@'..msg.from.username..'\n'
          return reply_msg(reply_id, text, ok_cb, false)
          end
 local reply_id = msg['id']
@@ -29,7 +29,7 @@ local reply_id = msg['id']
 if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
     local fwd = 'mate:'..msg.to.id
     redis:del(fwd)
-    local text = 'تم ☑️ فتح 🔓 اعاده توجيه'..'\n\n'..' 👮 MSG BY : @'..msg.from.username..'\n➖➖➖➖➖➖➖➖➖🙄ID MSG BY : '..msg.from.id..'\n➖➖➖➖➖➖➖➖➖🎩GROUP BY : '..msg.to.title..'\n'
+    local text = 'تم ☑️ فتح 🔓 اعاده توجيه'..'\n\n'..msg.to.title..'\n✈️MSG BY :-@'..msg.from.username..'\n'
     return reply_msg(reply_id, text, ok_cb, false)
 end
 
@@ -45,7 +45,7 @@ return {
         '^(قفل) (اعاده توجيه)$',
         '^(فتح) (اعاده توجيه)$'
     },
-run = h_k_a,
+run = sadikal_knani10,
 pre_process = pre_process 
 }
 end
