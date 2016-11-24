207b0a6eb6477ab65bbd88698a8d4c1deb9ce782
blackops97 <blackops97@users.noreply.github.com>
2016-11-24 10:43:35 +0000

    Update ar_version.lua

--- plugins/ar_version.lua
+++ plugins/ar_version.lua
@@ -12,7 +12,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-  local S = '🌐 #السورس KING TELE\n🔰 الاصدار v6.0 ☑️\n🌐 الموقع\n \nhttps://github.com/blackops97/KING_TELE\n🔧 لمطورين @illOlli \n @Sadikal_knani10 \n 🔧📱 قناة البوت @KINGTELE1'
+  local S = ' 👁‍🗨 سِـوَرَسِ KING TELE \n 🐯 الُاصّـ♔ـدِارَ :- الُثُالُثُ \n\n 🔭 الُمٌوَقًْع https://github.com/blackops97/KING_TELE \n\n ✈️ مٌطِوَرَيَنَ الُسِـوَرَسِ \n 🦁 بّـكِوَرَيَ :- @ll_B5 \n 🦁 صّـادِقً :- @sadikal_knani10 \n\n 🛰 قًنَاتْنَا ْعلُى الُتْلُيَ كِرَامٌ \n 👁‍🗨 @illOlli '
 reply_msg(reply_id, S, ok_cb, false)
 end
 
